207a8eed5636cc3debbfa2c14ef0267d7123b099
Michal Kubenka <mkubenka@gmail.com>
2016-09-09 01:39:58 +0200

    Use PATH for finding Bosun executable.

--- spec/acceptance/bosun_spec.rb
+++ spec/acceptance/bosun_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe 'bosun' do
-  describe command('/usr/local/bin/bosun -version') do
+  describe command('bosun -version') do
     its(:exit_status) { should eq 0 }
     its(:stdout) { should match /bosun version/ }
   end
